0a31a40d1de55fc8a3be091f1b87e1d81e08f3ca
Priyanshu Gupta <priyanshoon.pg@gmail.com>
2026-02-12 02:16:06 +0530

    repl added

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/priyanshoon/toy-interpreter/repl"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Toy Programming Language!\n",
+		user.Username)
+
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
