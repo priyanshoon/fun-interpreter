f0574e436f6e0c6066268815ecfedc545f17200e
Priyanshu Gupta <priyanshoon.pg@gmail.com>
2026-03-01 01:28:36 +0530

    ast for toy interpreter (in progress)

---
+++ ast/ast.go
@@ -0,0 +1,52 @@
+/*
+top-down parsers -> starts with constructing root node of AST and descends
+bottom-up parser -> starts with leaf (tokens) nodes and builds upwards
+*/
+package ast
+
+import (
+	"github.com/priyanshoon/toy-interpreter/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statement []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statement) > 0 {
+		return p.Statement[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
